23ff7680096e1a03f5e60a1d30110ed8e21e6a38
bohauhuang <xab9604030925@gmail.com>
2018-07-06 20:38:49 +0800

    0706 updated

---
+++ two sum/two sum.c
@@ -0,0 +1,15 @@
+// solved by brute force  84ms
+int* twoSum(int* nums, int numsSize, int target) {
+	static int ans[2];
+    int i = 0,j;
+    for(; i < numsSize ; i++){
+        for(j = i+1 ; (j < numsSize && j != i) ; j++){
+            if(nums[i]+nums[j] == target){
+                ans[0] = i;
+                ans[1] = j;
+                return two;
+            }
+        }
+    }
+    return 0;
+}
