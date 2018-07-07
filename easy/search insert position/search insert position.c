8c769d4269065ca4f5172bea50d061df10e2032b
bohauhuang <xab9604030925@gmail.com>
2018-07-07 22:21:44 +0800

    0707 updated

---
+++ easy/search insert position/search insert position.c
@@ -0,0 +1,9 @@
+int searchInsert(int* nums, int numsSize, int target) {
+    for(int i = 0 ; i < numsSize ; i++){
+        if(target == nums[i]) return i;
+    }
+    for(int i = 0 ; i < numsSize ; i++){
+        if(target < nums[i]) return i;
+    }
+    return numsSize;
+}
